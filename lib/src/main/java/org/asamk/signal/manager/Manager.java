dfc65bc7c2c34c71f1855853423eeaa5d836c299
DukhaSlayer <dukhaSlayer@pm.me>
2025-03-01 15:27:24 +0200

    ManagerImpl.close() (client shutdown) procedure improvement. Removing the risk of loosing messages.

--- lib/src/main/java/org/asamk/signal/manager/Manager.java
+++ lib/src/main/java/org/asamk/signal/manager/Manager.java
@@ -37,6 +37,7 @@ import org.asamk.signal.manager.api.PinLockedException;
 import org.asamk.signal.manager.api.RateLimitException;
 import org.asamk.signal.manager.api.ReceiveConfig;
 import org.asamk.signal.manager.api.Recipient;
+import org.asamk.signal.manager.api.RecipientAddress;
 import org.asamk.signal.manager.api.RecipientIdentifier;
 import org.asamk.signal.manager.api.SendGroupMessageResults;
 import org.asamk.signal.manager.api.SendMessageResult;
